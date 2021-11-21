affce16a0d33027ff0e3177b6d192070d4ae0902
Jobin Jacob <jobinjacob1810@gmail.com>
2021-11-22 04:13:44 +0530

    all questions added and optimised

--- app/src/main/java/com/jobin/chatbot/utils/ChatHandler.kt
+++ app/src/main/java/com/jobin/chatbot/utils/ChatHandler.kt
@@ -6,7 +6,17 @@ object ChatHandler {
         put(QuesAns.Questions.HELLO, QuesAns.Answers.QUES_HELLO)
         put(QuesAns.Questions.HI, QuesAns.Answers.QUES_HI)
         put(QuesAns.Questions.COST,QuesAns.Answers.QUES_COST)
+        put(QuesAns.Questions.DOCS,QuesAns.Answers.QUES_DOCS)
         put(QuesAns.Questions.SEATS,QuesAns.Answers.QUES_SEATS)
+        put(QuesAns.Questions.PROCS,QuesAns.Answers.QUES_PROCS)
+        put(QuesAns.Questions.ASS,QuesAns.Answers.QUES_ASS)
+        put(QuesAns.Questions.OUT,QuesAns.Answers.QUES_OUT)
+        put(QuesAns.Questions.FUR,QuesAns.Answers.QUES_FUR)
+        put(QuesAns.Questions.PRO,QuesAns.Answers.QUES_PRO)
+        put(QuesAns.Questions.BTECH,QuesAns.Answers.QUES_BTECH)
+        put(QuesAns.Questions.MTECH,QuesAns.Answers.QUES_MTECH)
+        put(QuesAns.Questions.PHD,QuesAns.Answers.QUES_PHD)
+        put(QuesAns.Questions.CAN,QuesAns.Answers.QUES_CAN)
     }
 
     fun getQuestions() : List<String> {
--- app/src/main/java/com/jobin/chatbot/utils/QuesAns.kt
+++ app/src/main/java/com/jobin/chatbot/utils/QuesAns.kt
@@ -7,6 +7,18 @@ object QuesAns {
         const val HI = "Hi"
         const val SEATS = "How many seats are available for admission process?"
         const val COST ="What is the cost for applying to the various programs?"
+        const val DOCS ="What are the documents to be submitted along with the admission form?"
+        const val PROCS ="]What are the further procedure? How will the acknowledgment process take place for the application?"
+        const val ASS ="How will the application be assessed for admission?"
+        const val OUT ="What are the different outcomes following our application?"
+        const val FUR ="What are the further steps to be taken once the admission is confirmed?"
+        const val PRO ="how do we proceed with the payment process?"
+        const val BTECH ="What is the eligibility criteria for B.Tech?"
+        const val MTECH ="What are the eligibility criteria for M.Tech?"
+        const val PHD="What is the eligibility criteria for PHD?"
+        const val CAN="What are the various processes for cancellation of Admission?"
+
+
     }
 
     object Answers {
@@ -15,6 +27,71 @@ object QuesAns {
         const val QUES_SEATS ="There are around 300 seats"
         const val QUES_COST ="The cost of applying for undergraduate programs is INR 1550, post –" +
                 " graduate programs is INR 2150 and doctoral programs is INR 3150."
+        const val QUES_DOCS ="Scan and attach your recent photograph and valid address proof along " +
+                "with the application form."
+        const val QUES_PROCS ="We will send you an acknowledgement e-mail, which will explain how to " +
+                "check the status of your application . We will call you in case we require any " +
+                "additional information."
+        const val QUES_ASS ="Once your application has been received, it will be considered and " +
+                "we might ask you to come and meet us for an interview/ group discussion / written test or undertake the" +
+                " same online. We will further analyze your application to ensure you to meet the eligibility criteria for" +
+                " the program(s) your applying " +
+                "for. We consider your academic history and relevant experience to ensure you can " +
+                "succeed in your program. Assessment process usually ranges between 2-10 working days. " +
+                "Once the process is complete, we will get touch with you and let you know the status of your application."
+        const val QUES_OUT ="We will get in touch with you and let you know the status of your application. " +
+                "The following will be the outcomes:\n" +
+                "“CONFIRMED – You have been granted confirmed seat and you can claim it by submitting" +
+                " the relevant documents and paying the tuition fee.\n" +
+                "PROVISIONAL-You have met some of the eligibility requirements for entry to" +
+                " your chosen program of study and we would like to offer you a provisional " +
+                "seat till you satisfy the rest of the requirements stipulated time frame .\n " +
+                "If you are unable to clear the eligibility required your admission will stand cancelled." +
+                "CONDITIONAL – You have to meet the conditions and approvals listed in your conditional " +
+                "offer to order to claim a formal offer from the university.\n" +
+                "DECLINED- If you don’t meet the entry requirements or all seats are taken, " +
+                "we might offer you alternative program."
+        const val QUES_FUR ="Once you accept our offer, we will require your documents and once the " +
+                "documents have been verified, you will be prepared to pay the tuition fees "
+        const val QUES_PRO ="We offer students friendly installment schemes and also give the facility " +
+                "of student loan. You can learn more about it on www.dypatil.edu."
+        const val QUES_BTECH =" The candidate should have passed HSC or its equivalent examination " +
+                "with Physics and Mathematics as compulsory subjects along with one of the Chemistry " +
+                "or Biotechnology or Biology or Technical Vocational subjects, and obtained at least" +
+                " 50% marks (at least 45% marks, in case of Disability candidates belonging to Maharashtra State only)" +
+                " in the above subjects taken together." +
+                " Or The candidate should have passed a Diploma in Engineering and Technology and " +
+                "obtained at least 50% marks (at least 45% marks, in case of Disability candidates " +
+                "belonging to Maharashtra State only)."
+        const val QUES_MTECH ="Candidates who have completed their BE / BTech in the relevant field" +
+                " of Engineering and Technology from All India Council for Technical Education or " +
+                "Central or State Government approved institutions or equivalent, with at least 50% " +
+                "marks (at least 45% marks in case of candidates of backward class categories)." +
+                "Obtained Non-Zero Positive Score in GATE are eligible for M.Tech degree courses.or" +
+                "For sponsored candidates, a minimum of two years of full-time work experience" +
+                " in a registered firm/company/industry / educational and/or research institute / any Government" +
+                " Department or Government Autonomous Organization in the relevant field in which " +
+                "admission is being sought."
+        const val QUES_PHD ="M.Tech / M.E in relevant discipline from an Institute / University recognized" +
+                " by AICTE / UGC / Central Government" +
+                "Admission to the Ph.D. courses is based on an entrance test followed by an interview." +
+                "Candidates with UGC-NET (including JRF), UGC-CSIR NET (including JRF), SLET, GATE, " +
+                "teacher fellowship holder or have passed M.Phil Program is exempted from Entrance " +
+                "Test but is required to appear for an interview." +
+                "Final Selection will be based on the overall merit index of the candidate which will" +
+                " be arrived at by taking into account the candidate’s performance in the previous " +
+                "university examinations at the Master’s level and the performance in the written " +
+                "test, research output and the performance in the personal interview."
+        const val QUES_CAN ="Full fees will be refunded if a candidate withdraws admission within 1 " +
+                "month from the date of securing admission. INR 2,000 will be deducted on account of " +
+                "administrative charges." +
+                "No fees will be refunded if a candidate withdraws admission after 1 month of securing admission." +
+                "For withdrawal, a handwritten application has to be submitted to the Admissions" +
+                " Office along with the fee receipt." +
+                "Refund /Cancellation process will be initiated only after the candidate has submitted" +
+                " the No dues form in the administrative department."
+
+
 
         const val DEFAULT = "Please select from the available questions"
     }
